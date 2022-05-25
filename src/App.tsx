683efee43ffa3c7051e136de4da13db08d948681
Rachid Lajmi <mosaixel.org@gmail.com>
2022-05-25 15:50:58 +0200

    bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Button>YES</Button>
     </div>
   );
 }
